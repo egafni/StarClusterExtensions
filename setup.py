4177fc440efa333a95a3b6307332ce1305276b21
Erik Gafni <egafni@gmail.com>
2016-02-07 17:27:11 -0800

    bump

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def all_files(path, num_root_dir_to_skip=1):
 
 
 setup(name='StarClusterExtensions',
-      version='0.3.6',
+      version='0.3.7',
       description="StarCluster Extensions",
       author='Erik Gafni',
       license='MIT',

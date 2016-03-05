32204eba34fd79aeb66a0f85387c5bd770bc4a97
Erik Gafni <egafni@gmail.com>
2016-03-04 16:48:38 -0800

    fix sge_plus

--- sce/plugins/sge_plus/plugin.py
+++ sce/plugins/sge_plus/plugin.py
@@ -20,7 +20,8 @@ import subprocess as sp
 
 from starcluster.logger import log
 from starcluster.clustersetup import ClusterSetup
-
+from sce.utils.misc import trace
+from sce.utils.node import execute
 
 opj = os.path.join
 
@@ -31,20 +32,22 @@ class SGE_Plus_Setup(ClusterSetup):
         self.master_slots = master_slots
         super(SGE_Plus_Setup, self).__init__(**kwargs)
 
+    @trace
     def run(self, nodes, master, user, user_shell, volumes):
         log.info('Running SGE Plus')
 
         # update qconf complex list to make h_vmem and num_proc consumable
         log.info('Update complex configuration')
         master.ssh.put(opj(self.data_path, 'qconf_c'), '/tmp/qconf_c')
-        master.ssh.execute('qconf -Mc /tmp/qconf_c')
+        execute(master, 'qconf -Mc /tmp/qconf_c')
         log.info('Update ms configuration')
         master.ssh.put(opj(self.data_path, 'msconf'), '/tmp/msconf')
-        master.ssh.execute('qconf -Msconf /tmp/msconf')
+        execute(master, 'qconf -Msconf /tmp/msconf')
 
         for node in nodes:
             self.on_add_node(node, nodes, master, user, user_shell, volumes)
 
+    @trace
     def _update_complex_list(self, node):
         """
         Sets a node's h_vmem and num_proc complex values
@@ -52,18 +55,20 @@ class SGE_Plus_Setup(ClusterSetup):
         :param node: The node to update
         """
         log.info('Updating complex values for {0}'.format(node))
-        memtot = int(node.ssh.execute('free -g|grep Mem:|grep -oE "[0-9]+"|head -1')[0])
-        num_proc = self.master_slots if node.is_master() else node.ssh.execute('nproc')[0]
+        print execute(node, 'free -g|grep Mem:|grep -oE "[0-9]+"|head -1')
+        memtot = execute(node, 'free -g|grep Mem:|grep -oE "[0-9]+"|head -1')[0]
+        num_proc = self.master_slots if node.is_master() else execute(node, 'nproc')[0]
         # scratch_mb= sp.check_output('df |grep scratch',shell=True).split()[3]
-        scratch_mb=0
+        scratch_mb = 0
 
         if node.is_master():
-            num_proc = int(num_proc/2)
+            num_proc = int(num_proc / 2)
 
-        node.ssh.execute(
-            "qconf -rattr exechost complex_values slots={num_proc},num_proc={num_proc},sce_mem={mem}g,sce_scratch={scratch_mb} {node}".format(
-                mem=memtot, num_proc=num_proc, node=node.alias, scratch_mb=scratch_mb)
+        execute(node,
+                "qconf -rattr exechost complex_values slots={num_proc},num_proc={num_proc},sce_mem={mem}g,sce_scratch={scratch_mb} {node}".format(
+                    mem=memtot, num_proc=num_proc, node=node.alias, scratch_mb=scratch_mb)
         )
 
+    @trace
     def on_add_node(self, node, nodes, master, user, user_shell, volumes):
         self._update_complex_list(node)
